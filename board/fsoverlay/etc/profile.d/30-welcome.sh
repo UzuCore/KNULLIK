9da0e43e03b0c9f8f60afcf57b3c6c503e337789
uzucore <byunjaeil@gmail.com>
2026-05-30 15:15:23 +0900

    Apply Korean boot UX, PortMaster, ES, and emulator fixes

--- board/fsoverlay/etc/profile.d/30-welcome.sh
+++ board/fsoverlay/etc/profile.d/30-welcome.sh
@@ -1,10 +1,10 @@
 # Add KNULLI logo and some alias, sourcing of $HOME/.bashrc can be added to $HOME/.profile
 echo 
-echo " _  ___   _ _   _ _     _     ___ "
-echo "| |/ / \ | | | | | |   | |   |_ _|"
-echo "| ' /|  \| | | | | |   | |    | | "
-echo "| . \| |\  | |_| | |___| |___ | | "
-echo "|_|\_\_| \_|\___/|_____|_____|___|"
+echo "██   ██ ███    ██ ██    ██ ██      ██      ██" 
+echo "██  ██  ████   ██ ██    ██ ██      ██      ██" 
+echo "█████   ██ ██  ██ ██    ██ ██      ██      ██"
+echo "██  ██  ██  ██ ██ ██    ██ ██      ██      ██"
+echo "██   ██ ██   ████  ██████  ███████ ███████ ██"
 echo
 knulli-info 2>/dev/null
 echo "OS version: $(knulli-version)"
